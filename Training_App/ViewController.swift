87ac44b98399a08794207646b8ef2e05e34917d6
JohnnieV <johnnie.v1986@gmail.com>
2023-01-14 17:20:24 +0400

    Initial Commit

---
+++ Training_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Training_App
+//
+//  Created by Владимир Смирнов on 14.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
